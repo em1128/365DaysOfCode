c86d820ad9da43e23b5eb7b34a8530f323c64d48
em1128 <75158913+em1128@users.noreply.github.com>
2023-07-31 22:09:39 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie();
+    cout.tie();
+    int N, K;
+
+	queue<int> q;
+	cin >> N >> K;
+ 
+	for (int i = 1; N >= i; i++) {
+		q.push(i);
+	}
+	
+	cout << "<";
+	while (!q.empty()) {
+		for (int i = 0; i < K - 1; i++) {
+			q.push(q.front());
+			q.pop();
+		}
+ 
+		cout << q.front();
+		q.pop();
+ 
+		if (!q.empty()) {
+			cout << ", ";
+		}
+ 
+	}
+	cout << ">" << endl;
+ 
+	return 0;
+
+}
